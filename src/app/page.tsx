e98b81e95bc87f09bd073836dd892a08543d4e70
Pedro Henrique <130567453+pedrohbragac@users.noreply.github.com>
2024-08-13 19:30:09 -0300

    add conent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import { Hero } from "@/sections/Hero";
 import { Header } from "../sections/Header";
+import { LogoTicker } from "@/sections/LogoTicker";
+import { ProductShowcase } from "../sections/ProductShowcase";
+import { Pricing } from "@/sections/Pricing";
 
 export default function Home() {
   return <>
     <Header />
     <Hero />
+    <LogoTicker />
+    <ProductShowcase />
+    <Pricing />
   </>
 }
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,21 @@
+import productImage from "@/assets/product-image.png"
+import Image from "next/image";
+import pyramidImage from "@/assets/pyramid.png";
+import tubeImage from "@/assets/tube.png";
+
 export const ProductShowcase = () => {
-  return null;
+  return <section className="bg-gradient-to-b from-[#FFFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
+    <div className="container  max-w-none">
+      <div className=" md:max-w-[540px] lg:max-w-[800px] mx-auto text-center space-y-6">
+        <div className="tag">Boost your productivity</div>
+        <h2 className="section-title">A more effective way to track progress</h2>
+        <p className="section-description">Effortlessly turn your ideas into a fully functional, responsive, no-code SaaS website in just minutes with the set of free components for Framer.</p>
+      </div>
+      <div className="relative">
+      <Image src={productImage} alt="Product Image" className="" />
+      <Image src={pyramidImage} alt="Pyramid Image" className="hidden md:block absolute top-0 -right-24" height={262} width={262}/>
+      <Image src={tubeImage} alt="Tube Image" className="hidden md:block absolute bottom-20 -left-24" height={262} width={262}/>
+      </div>
+    </div>
+  </section>;
 };
